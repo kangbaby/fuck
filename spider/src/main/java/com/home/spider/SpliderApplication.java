a8425e1eb163559bc9f6b1e4a276165421e0e584
kangbaby <17799395958@163.com>
2017-06-25 19:58:20 +0800

    spider
    first commit

---
+++ spider/src/main/java/com/home/spider/SpliderApplication.java
@@ -0,0 +1,17 @@
+package com.home.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * boot
+ * Created by ALIENWARE on 2017/6/25.
+ */
+@SpringBootApplication
+public class SpliderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpliderApplication.class, args);
+    }
+
+}
